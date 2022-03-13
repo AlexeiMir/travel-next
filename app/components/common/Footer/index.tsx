19a4f20c0550fe9e892bb620440eb766ab22e1dd
AlexeiMir <alex_16.07@mail.ru>
2022-03-13 11:14:17 +0700

    Init project

---
+++ app/components/common/Footer/index.tsx
@@ -0,0 +1,45 @@
+import { useRouter } from 'next/router';
+import styles from './styles.module.scss';
+import {FC} from 'react';
+
+
+const navItems = [
+	{
+		icon: 'home',
+		link: '/',
+	},
+	{
+		icon: 'explore',
+		link: '/explore',
+	},
+	{
+		icon: 'place',
+		link: '/place/kyoto',
+	},
+	{
+		icon: 'person_outline',
+		link: '/profile',
+	},
+]
+
+export const Footer: FC = () => {
+    const { push, pathname } = useRouter();
+
+    return (
+        <footer className={styles.footer}>
+            <nav>
+                {navItems.map(item => (
+                   <button
+                   className={pathname === item.link ? styles.active : ''}
+                   onClick={() => push(item.link)}
+                   >
+                       <span className='material-icons-outlined'>{item.icon}</span>
+
+                   </button> 
+                ))}
+            </nav>
+
+        </footer>
+    )
+
+}
